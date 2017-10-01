db5a68b18181ae4046968b43c8c3b8e77268cf02
Daniel McDonald <mcdonald.daniel.james@gmail.com>
2017-10-01 09:15:11 -0700

    refactor all classes/interfaces into single file
    This makes it easier to copy/paste the entire thing into things like
    codinggame, which requires a single file for submission.

--- src/MonteCarlo/MonteCarloTreeSearch.cs
+++ src/MonteCarlo/MonteCarloTreeSearch.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 
 namespace MonteCarlo
 {
+    public interface IAction {}
+
+    public interface IPlayer {}
+
+    public interface IState<TPlayer, TAction>
+    {
+        IState<TPlayer, TAction> Clone();
+
+        TPlayer CurrentPlayer { get; }
+
+        IList<TAction> Actions { get; }
+
+        void ApplyAction(TAction action);
+
+        double GetResult(TPlayer forPlayer);
+    }
+
     public class MonteCarloTreeSearch
     {
         private class Node<TPlayer, TAction> : IMctsNode<TAction> where TPlayer : IPlayer where TAction: IAction
@@ -119,6 +135,44 @@ namespace MonteCarlo
             root.BuildTree((numIterations, elapsedMs) => numIterations < maxIterations && elapsedMs < timeBudget);
             return root.Children
                 .OrderByDescending(n => n.NumRuns);
+        }
+    }
+
+    public interface IMctsNode<TAction> where TAction : IAction
+    {
+        TAction Action { get; }
+
+        int NumRuns { get; }
+
+        double NumWins { get; }
+    }
+
+    internal static class CollectionExtensions
+    {
+        private static Random _random = new Random();
+
+        public static T RandomChoice<T>(this ICollection<T> source, Random random = null)
+        {
+            var i = (random ?? _random).Next(source.Count);
+            return source.ElementAt(i);
+        }
+
+        public static T MaxElementBy<T>(this IEnumerable<T> source, Func<T, double> selector)
+        {
+            var currentMaxElement = default(T);
+            var currentMaxValue = double.MinValue;
+
+            foreach (var element in source)
+            {
+                var value = selector(element);
+                if (currentMaxValue < value)
+                {
+                    currentMaxValue = value;
+                    currentMaxElement = element;
+                }
+            }
+
+            return currentMaxElement;
         }
     }
 }
